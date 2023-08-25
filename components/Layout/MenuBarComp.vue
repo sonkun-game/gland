1057f3846efbeb9df82357f8d99a7748b0d11606
Son <mikechael74@gmail.com>
2023-08-25 07:15:00 +0700

    add department

--- components/Layout/MenuBarComp.vue
+++ components/Layout/MenuBarComp.vue
@@ -98,12 +98,10 @@
                     placeholder="Nhập tên phòng ban"></InputField>
                 </div>
                 <!-- Modal footer -->
-
                 <div class="flex justify-end items-center p-6 space-x-2 border-gray-200 rounded-b dark:border-gray-600">
                   <button :data-modal-hide="department.id"
                     class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900">
                     Hủy bỏ</button>
-
                   <button :data-modal-hide="department.id" @click="addDepartment()"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lưu</button>
                 </div>
@@ -202,7 +200,7 @@ export default {
         {
           icon: "fa-solid fa-address-card",
           name: "Phòng ban",
-          link: "#",
+          link: "/main/total/departments",
           id: "department-1",
         },
       ],
@@ -247,10 +245,12 @@ export default {
     },
     addDepartment() {
       var name = document.getElementById(this.department.nameID).value;
+      var key = name.replace(/\s/g, "").toLowerCase();
+      let id = Math.floor(Math.random() * 100) + 1;
       this.loadedDepartment.push({
         icon: "fa-solid fa-address-card",
         name: name,
-        link: "#B",
+        link: "/main/total/departments/" + key + "/" + id,
         id: "department-1" + this.common.listDepartmentKey,
       });
       this.common.listDepartmentKey++;
