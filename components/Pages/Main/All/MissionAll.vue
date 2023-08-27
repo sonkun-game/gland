769e92ea74e86a8642e7765f4d8ead57c5a530cf
datct15 <kiwisun1998@gmail.com>
2023-08-27 17:52:28 +0700

    add createMission

--- components/Pages/Main/All/MissionAll.vue
+++ components/Pages/Main/All/MissionAll.vue
@@ -10,8 +10,8 @@
       <ModalHeader head="Quản lý nhiệm vụ" :modalId="createMission.id"></ModalHeader>
       <!-- Modal body -->
       <div>
-        <InputField label="Tên nhiệm vụ" styleClass="px-4"></InputField>
-        <InputField styleClass="px-4" label="Mô tả" typeInput="textarea"></InputField>
+        <InputField id="all_mission_txtName" label="Tên nhiệm vụ" styleClass="px-4"></InputField>
+        <InputField id="all_mission_txtDes" styleClass="px-4" label="Mô tả" typeInput="textarea"></InputField>
       </div>
       <!-- Modal footer -->
 
@@ -20,7 +20,7 @@
           class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900">
           Hủy bỏ</button>
 
-        <button :data-modal-hide="createMission.id"
+        <button :data-modal-hide="createMission.id" @click="createMissionFunct(storeId)"
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lưu</button>
       </div>
     </ModalContainer>
@@ -242,7 +242,7 @@
   </div>
 </template>
 <script>
-import { getAllMission, getAllMissionPaging } from '../../../../static/mission/api';
+import {createMissionAll, getAllMission, getAllMissionPaging} from '../../../../static/mission/api';
 import moment from "moment";
 export default {
   data() {
@@ -288,6 +288,9 @@ export default {
   methods: {
     formatDate(date) {
       return moment(date).format('DD/MM/YYYY HH:mm');
+    },
+    createMissionFunct(storeId){
+      createMissionAll(storeId);
     }
   },
   async mounted() {
--- static/mission/api.js
+++ static/mission/api.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { v4 as uuidv4 } from 'uuid';
+
 export async function getAllMission(storeId) {
   let url = process.env.API_URL + 'api-mission/?storeId=' + 1;
   // if (pageNum != null)
@@ -36,4 +38,30 @@ export async function getAllMissionPaging() {
     console.error('Error fetching data:', error);
     throw error;
   }
+}
+
+export async function createMissionAll(storeId) {
+  var name = document.getElementById("all_mission_txtName").value;
+  var description = document.getElementById("all_mission_txtDes").value;
+  var keyUUID= uuidv4();
+  axios({
+
+    method: 'post',
+    url: 'http://103.142.26.40:8080/Spa/api-mission/create?storeId=' + storeId,
+    responseType: 'json',
+    data: {
+      name: name,
+      storeId: storeId,
+      description: description,
+      keyUUID: keyUUID
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem("jwt")
+    }
+  }).then(function (response) {
+    console.log(response);
+    alert(response.data.message);
+    location.reload()
+  });
 }
