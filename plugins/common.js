96ad89379a9454ce46fabab756f3c908314e3c4d
Son <mikechael74@gmail.com>
2023-08-30 07:27:48 +0700

    update common.js

--- plugins/common.js
+++ plugins/common.js
@@ -5,41 +5,47 @@
  */
 
 export const Common = {
-    /**
-     * Get 1 chuỗi key tránh bị trùng lặp ID,...
-     * @param {*} length
-     * @returns
-     */
-    getRandomKey(length=8) {
-        let randomNumber = Math.floor(Math.random() * 10000) + 1;
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        let randomText = '';
-        for (let i = 0; i < length; i++) {
-            const randomIndex = Math.floor(Math.random() * characters.length);
-            randomText += characters.charAt(randomIndex);
-        }
-        return randomText + randomNumber;
-    },
-    /**
-     * Xóa unicode, lowercase, xóa space, trả về dộ dài theo y/c
-     * @param {*} length
-     */
-    getKeyFromText(key="",length=6) {
-        key = key.replace(/\s/g, "");
-        key = key.replace(/[^\x00-\x7F]/g, "").toLowerCase();
-        if(key.length > length) {
-            return key.substring(0,length)
-        }
-        return key;
-    },
-    /**
-     * get đường link các trang, dùng để redirect khi bấm vào link,...
-     * @param {*} url
-     * @returns
-     */
-    baseUrl(url) {
-      let protocol = window.location.protocol;
-      let host = window.location.host;
-      return protocol + "//" + host + url;
+  /**
+   * Get 1 chuỗi key tránh bị trùng lặp ID,...
+   * @param {*} length
+   * @returns
+   */
+  getRandomKey(length = 8) {
+    let randomNumber = Math.floor(Math.random() * 10000) + 1
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    let randomText = ''
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length)
+      randomText += characters.charAt(randomIndex)
     }
+    return randomText + randomNumber
+  },
+  /**
+   * Xóa unicode, lowercase, xóa space, trả về dộ dài theo y/c
+   * @param {*} length
+   */
+  getKeyFromText(key = '', length = 6) {
+    key = key.replace(/\s/g, '')
+    key = key.replace(/[^\x00-\x7F]/g, '').toLowerCase()
+    if (key.length > length) {
+      return key.substring(0, length)
+    }
+    return key
+  },
+  /**
+   * get đường link các trang, dùng để redirect khi bấm vào link,...
+   * @param {*} url
+   * @returns
+   */
+  baseUrl(url) {
+    if (typeof window !== 'undefined') {
+      let protocol = window.location.protocol
+      let host = window.location.host
+      return protocol + '//' + host + '/' + url
+    } else {
+      console.log("Window is undefined !");
+    }
+    return ''
+  },
 }
