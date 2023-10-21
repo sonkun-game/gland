1d23d776377a37bd4c797737dfba2ddef6946ed7
Hoàng Sơn <107675094+sonkun-game@users.noreply.github.com>
2023-10-21 09:32:41 +0700

    remove comment

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,13 +18,6 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      // {
-      //   rel: 'stylesheet',
-      //   href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-      //   integrity:
-      //     'sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM',
-      //   crossorigin: 'anonymous',
-      // },
       {
         rel: 'stylesheet',
         href: 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css',
@@ -34,12 +27,6 @@ export default {
       {
         src: 'https://code.jquery.com/jquery-3.7.0.min.js',
       },
-      // {
-      //   src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
-      //   integrity:
-      //     'sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz',
-      //   crossorigin: 'anonymous',
-      // },
       {
         src: 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js',
       },
